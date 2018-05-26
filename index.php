fad0f7323f0f3b6efe5b0b6549c507b98e225472
Adam <anolan19@terpmail.umd.edu>
2018-05-26 14:47:24 -0400

    initial commit

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+require_once "sup.php";
+
+echo generatePage("<h1>hellow world</h1><pre>
+
+
+
+<h3>goodbye world</h3>
+</pre>");
---
+++ sup.php
@@ -0,0 +1,43 @@
+<?php
+
+	function generatePage($body, $title="Dungeon Log System") {
+    	    $page = <<<EOPAGE
+<!doctype html> 
+	<html> 
+ <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">  
+	    <head>  
+	        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" /> 
+	        <title>$title</title>    
+	    </head> 
+	    <nav class="navbar sticky-top navbar-light bg-light">
+  <a class="navbar-brand" href="#">[Dungeon log]</a>
+</nav>
+	             
+	    <body> 
+            $body 
+	     <br>   
+	    </body> 
+	</html>
+EOPAGE;
+
+	    return $page;
+	}
+
+	function connect(){
+
+
+    $servername = "localhost";
+    $username = "dbuser";
+    $password ="goodbyeWorld";
+	    $db="applicationdb";
+
+    $conn = new mysqli($servername, $username, $password, $db);
+
+
+    if ($conn->connect_error) {
+	        die("Connection failed: " . $conn->connect_error);
+	    } else{
+	        return $conn;
+	    }
+	}
+	?>
